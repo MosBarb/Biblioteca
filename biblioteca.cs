b816780e8670aad5292671df94c71bb8eab081f3
Mose Barbieri <MOSE.BARBIERI@local.ittcesena.it>
2022-11-17 09:06:37 +0100

    Versione biblioteca 2

--- biblioteca.cs
+++ biblioteca.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace BibliotecaScuoleMedie
 {
-    internal class biblioteca
+    internal class biblioteca : libro
     {
+        internal string nome { get; set; }
+        internal string indirizzo { get; set; }
+        internal string OrarioApertura { get; set; }
+        internal string OrarioChiusura { get; set; }
+
+        internal biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+
+            
+        }
     }
 }
