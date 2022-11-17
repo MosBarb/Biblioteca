6fc17962e19af19bd6aff0b7bbd963a7ec0230ce
Mose Barbieri <MOSE.BARBIERI@local.ittcesena.it>
2022-11-17 08:56:02 +0100

    Versione parte 1 classe Libro

---
+++ libro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaScuoleMedie
+{
+    internal class libro
+    {
+        internal int pagine { get; set; }
+        internal string titolo { get; set; }
+        internal string autore { get; set; }
+
+        internal libro()
+        {
+            this.pagine = pagine;
+            this.titolo = titolo;
+            this.autore = autore;
+
+
+        }
+
+        static override toString()
+        {
+            return string;
+        }
+
+       
+    }
+
+    
+}
